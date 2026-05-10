d598898452b0a6a6b198743e14f738d428c851ba
Noah-droid <noahtochukwu10@gmail.com>
2026-05-10 02:46:16 +0100

    Clean UI and session auth architecture

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf-8'
 
 setup(
     name="django-live-logs",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
